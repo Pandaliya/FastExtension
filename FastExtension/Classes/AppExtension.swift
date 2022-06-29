4277645a362c914e2e4e80bad5bfa668fc8c852c
zhangpan <zhangpan@cls.cn>
2022-06-29 17:14:19 +0800

    pod 0.1.0

---
+++ FastExtension/Classes/AppExtension.swift
@@ -0,0 +1,13 @@
+//
+//  AppExtension.swift
+//  FastExtension
+//
+//  Created by pan zhang on 2022/6/29.
+//
+
+extension FastExtensionWrapper where Base: UIApplication {
+    public static var appVersion:String {
+        return ""
+    }
+}
+
---
+++ FastExtension/Classes/FastExtension.swift
@@ -0,0 +1,35 @@
+//
+//  FastExtension.swift
+//  FastExtension
+//
+//  Created by pan zhang on 2022/6/29.
+//
+
+import Foundation
+import UIKit
+
+public struct FastExtensionWrapper<Base> {
+    public let base: Base
+    public init(_ base: Base) {
+        self.base = base
+    }
+}
+
+public protocol FastExtensionCompatible: AnyObject { }
+
+extension FastExtensionCompatible {
+    public var fe: FastExtensionWrapper<Self> {
+        get { return FastExtensionWrapper(self) }
+        set { }
+    }
+    
+    public static var fe: FastExtensionWrapper<Self>.Type {
+        get { return FastExtensionWrapper<Self>.self}
+        set {}
+    }
+}
+
+
+extension UIApplication: FastExtensionCompatible { }
+
+
