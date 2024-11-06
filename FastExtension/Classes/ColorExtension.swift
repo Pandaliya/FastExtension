08f3806c65fc61622e4f7a48457977d504d615f5
zhangpan <zhangpan@cls.cn>
2024-11-06 18:13:53 +0800

    update

--- FastExtension/Classes/ColorExtension.swift
+++ FastExtension/Classes/ColorExtension.swift
@@ -19,5 +19,13 @@ extension FastExtensionWrapper where Base: UIColor {
             alpha: 1
         )
     }
+    
+    
+    public static func hex(_ hex: Int, alpha: CGFloat = 1.0) -> UIColor {
+        let red = CGFloat((hex >> 16) & 0xFF) / 255.0
+        let green = CGFloat((hex >> 8) & 0xFF) / 255.0
+        let blue = CGFloat(hex & 0xFF) / 255.0
+        return UIColor.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
 }
 
